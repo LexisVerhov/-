3fe32987d0eb1cb564d1f6c68dc7910894d81478
LexisVerhov <lexisverhov@gmail.com>
2019-03-31 17:05:04 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -5,8 +5,8 @@ import './style.css';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = "<button style=\"height:50px; width: 150px;\" id=\"stat\">Статистика</button><div id=\"stat-div\"></div>";
 
-document.getElementById("stat").addEventListener("click", function(event){
-    document.getElementById("stat-div").innerHTML = "<b>Мышь</b><br>" +
+document.getElementById("stat").addEventListener("click", function (event) {
+  document.getElementById("stat-div").innerHTML = "<b>Мышь</b><br>" +
     "Скорость движения мыши: " + mouseSpeedAvr(a_mouse_move_path) + "<br>" +
     "Скорость клика левой кнопкой мыши: " + arrayAvr(a_mouse_button_left_speed) + "<br>" +
     "Скорость клика средней кнопкой мыши: " + arrayAvr(a_mouse_button_middle_speed) + "<br>" +
@@ -16,8 +16,7 @@ document.getElementById("stat").addEventListener("click", function(event){
 
     "<b>Клавиатура</b>" + "<br>" +
     "===============" + "<br>" +
-    "Скорость нажатия клавиш: " + "<br>" +
-    "Скорость между печатью символов: " + "<br>";
+    "Скорость печати: " + keyboard_avr_speed_press(a_keyboard_keyspeed) + "<br>";
 });
 
 
@@ -48,26 +47,25 @@ function arrayAvr(array) {
 }
 
 function mouseSpeedAvr(array) {
-    var speed_array = [];
-    var speed = 0;
-    var y;
-    var x;
-    var distance = 0;
-    for (var i = 1; i < array.length; i++) {
-        x = array[i].x - array[i-1].x;
-        y = array[i].y - array[i-1].y;
-        speed = Math.sqrt( x*x + y*y ) / (array[i].time - array[i-1].time);
-        speed_array.push(speed);
-        }
-    
-    var sum = 0;
-    for (var i = 0; i < speed_array.length; i++) {
-        sum += speed_array[i];
-        }
+  var speed_array = [];
+  var speed = 0;
+  var y;
+  var x;
+  var distance = 0;
+  for (var i = 1; i < array.length; i++) {
+    x = array[i].x - array[i - 1].x;
+    y = array[i].y - array[i - 1].y;
+    speed = Math.sqrt(x * x + y * y) / (array[i].time - array[i - 1].time);
+    speed_array.push(speed);
+  }
 
-    return sum / speed_array.length;
-    }
+  var sum = 0;
+  for (var i = 0; i < speed_array.length; i++) {
+    sum += speed_array[i];
+  }
 
+  return sum / speed_array.length;
+}
 
 document.addEventListener("mousedown", function (event) {
   if (event.which == 1) {
@@ -86,7 +84,6 @@ document.addEventListener("mousedown", function (event) {
     mouse_button_right.y = event.pageY;
   }
 });
-
 
 document.addEventListener("mouseup", function (event) {
   if (event.which === 1) {
@@ -109,9 +106,33 @@ document.addEventListener("mouseup", function (event) {
   }
 });
 
-
 document.addEventListener("mousemove", function (event) {
-  var distance_struct = {x:event.clientX, y:event.clientY, time:Date.now()};
+  var distance_struct = { x: event.clientX, y: event.clientY, time: Date.now() };
   a_mouse_move_path.push(distance_struct);
 
 });
+
+
+var a_keyboard_keyspeed = [];
+
+document.addEventListener("keydown", function (event) {
+    a_keyboard_keyspeed.push(Date.now());
+})
+
+// когда будем считать - расстояния больше 3 секунд в учет не берем между символами
+function keyboard_avr_speed_press(array){
+      var speed_array = [];
+      var time = 0;
+      for (var i = 1; i < array.length; i++) {
+          time = array[i] - array[i-1];
+          if(time < 3000)
+              speed_array.push(time);
+      }
+
+      var sum = 0;
+      for (var i = 0; i < speed_array.length; i++) {
+        sum += speed_array[i];
+      }
+
+      return sum / speed_array.length;
+    }
